6dbf27b367b37fc5ef2801ba20ce936d4754106b
河村 泰佑 <kata.kawa813@gmail.com>
2021-05-27 21:17:40 +0900

    add

---
+++ pages/articles/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="">
+    <h1>記事一覧</h1>
+    <ui
+      ><li v-for="(c, index) in content" :key="index">
+        <nuxt-link :to="c.path">{{ c.title }}</nuxt-link>
+      </li></ui
+    >
+  </div>
+</template>
+<script>
+export default {
+  // asyncDataはサーバ側の処理
+  async asyncData({ store, $content, params }) {
+    const content = await $content('articles')
+      .only(['title', 'path'])
+      .sortBy('createdAt', 'desc')
+      .skip(0)
+      .limit(15)
+      .fetch()
+
+    return {
+      content,
+    }
+  },
+}
+</script>
