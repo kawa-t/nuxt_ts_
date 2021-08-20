72b6a25aca8e301e59ff646105b30c8fe4f501b4
河村 泰佑 <kata.kawa813@gmail.com>
2021-08-21 01:35:54 +0900

    test change

--- plugins/prism/index.ts
+++ plugins/prism/index.ts
@@ -1,9 +1,12 @@
 import 'clipboard'
 import Prism from 'prismjs'
+
+// eslint-disable-next-line prettier/prettier
 import 'prismjs/plugins/toolbar/prism-toolbar'
 import 'prismjs/plugins/toolbar/prism-toolbar.css'
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
 import 'prismjs/themes/prism-okaidia.css'
 import 'prismjs/components/prism-bash'
 import 'prismjs/components/prism-typescript'
+
 export default Prism
--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Async from '@/components/asyncPractice.vue'
 
-describe('Logo', () => {
+describe('Async', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Async)
     expect(wrapper.vm).toBeTruthy()
   })
 })
