56c09b49c79fa7dbc8e3fa90f16e52c3a1f6d1ce
河村 泰佑 <kata.kawa813@gmail.com>
2021-09-28 22:36:46 +0900

    dd

--- pages/minipractice/index.vue
+++ pages/minipractice/index.vue
@@ -8,7 +8,7 @@
   </div>
 </template>
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Watch } from 'vue-property-decorator'
 import TodoStorage from './todoStorage'
 import { State, TodoItemindex } from '@/types/TodoTypes'
 
@@ -29,8 +29,9 @@ export default class TodoComponent extends Vue {
   private current: State = State.All
 
   private get filteredTodos() {
-    return this.todos.filter(t =>
-    this.current === State.All ? true : this.current === t.state)
+    return this.todos.filter((t) =>
+      this.current === State.All ? true : this.current === t.state
+    )
   }
 
   private created() {
@@ -38,22 +39,22 @@ export default class TodoComponent extends Vue {
   }
 
   // private addTodo()
-  private addTodo(){
+  private addTodo() {
     // HTML要素のname
     const name = this.$refs.name as HTMLInputElement
-    if(!name.value.length) {
+    if (!name.value.length) {
       return false
     }
     this.todos.push({
       id: todoStorage.nextId,
       name: name.value,
-      state: State.Working
+      state: State.Working,
     })
     name.value = ''
   }
 
   // 削除
-  private removeTodo(todo:TodoItemindex) {
+  private removeTodo(todo: TodoItemindex) {
     const index = this.todos.indexOf(todo)
     this.todos.splice(index, 1)
   }
@@ -63,7 +64,9 @@ export default class TodoComponent extends Vue {
     todo.state = todo.state === State.Working ? State.Done : State.Working
   }
 
-  @Watch('todos', {deep: true}){
-
+  @Watch('todos', { deep: true })
+  private onTodoChange(todos: TodoItemindex[]) {
+    todoStorage.save(todos)
   }
 }
+</script>
