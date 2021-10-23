5e987e795f9d3c9a039896a1f41e9f106b4b4c6a
河村 泰佑 <kata.kawa813@gmail.com>
2021-10-23 21:22:39 +0900

    practice

---
+++ pages/classcomponent/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import Component from 'vue-class-component'
+import { Vue, Watch } from 'vue-property-decorator'
+
+@Component({
+  components: {},
+})
+export default class ClassApp extends Vue {
+  @Watch("count", {immediate: true})
+}
+</script>
