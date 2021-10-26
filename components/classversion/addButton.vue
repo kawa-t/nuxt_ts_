a9a800503f5bd0619553b516c6c22edd97c60483
河村 泰佑 <kata.kawa813@gmail.com>
2021-10-26 21:28:57 +0900

    practice

--- components/classversion/addButton.vue
+++ components/classversion/addButton.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <div>{{ count }}</div>
     <button>@emitで親に伝える</button>
   </div>
 </template>
+<script lang="ts">
+import Component from 'vue-class-component'
+import { Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Display extends Vue {
+  @Prop()
+  // 感嘆符(!)を付けることで、TypeScriptに値が確実に割り当てられいる事を伝えられる。
+  public count!: number
+}
+</script>
