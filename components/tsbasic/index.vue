0423950db23b37962e429d9a7217baa794294962
河村 泰佑 <kata.kawa813@gmail.com>
2021-10-30 22:21:12 +0900

    practice

---
+++ components/tsbasic/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div><button @click="typeAssertClick">TEST</button></div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class TsBasic extends Vue {
+  typeAssertClick() {
+    // OK
+    this.typeAssert('100')
+
+    // 数値はだめ
+    // this.typeAssert(100)
+
+    // asで明示的にすれば変えられる
+    this.typeAssert(100 as any)
+  }
+
+  typeAssert(result: string) {
+    return `結果は${result}です。`
+  }
+}
+</script>
+<style></style>
